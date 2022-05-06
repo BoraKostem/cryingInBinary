fa3d196a3388d8af40296d4a832282d0c09bd674
AlperKandemir1 <alperkandemir97@gmail.com>
2022-05-06 23:39:46 +0300

    migratins

--- appointment/database/migrations/2014_10_12_000000_create_users_table.php
+++ appointment/database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,14 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+
+            $table->integer('role_id');
+            $table->string('address')->nullable();
+            $table->string('phone_number')->nullable();
+            $table->string('department')->nullable();
+            $table->string('image')->nullable();
+            $table->text('description')->nullable();
+            $table->string('education')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
